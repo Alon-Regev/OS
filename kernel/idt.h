6f16db53ea636364622a798baaeb4c967a8f1d69
AlonRegev <alonr3704@gmail.com>
2022-06-30 00:04:29 +0300

    implemented interrupt service routines

--- kernel/idt.h
+++ kernel/idt.h
@@ -20,8 +20,8 @@ typedef struct idt_entry_struct idt_entry;
 // idt pointer struct suitable for 'lidt' instruction
 struct idt_ptr_struct
 {
-   uint16_t limit;
-   uint32_t addr;
+    uint16_t limit;
+    uint32_t addr;
 } __attribute__((packed));
 typedef struct idt_ptr_struct idt_ptr;
 
@@ -39,5 +39,53 @@ void init_idt();
 // input: interrupt number, values to fill entry.
 // return: none
 void idt_set_gate(uint8_t num, uint32_t addr, uint16_t selector, uint8_t flags);
+
+// type for the stack's state when entering the interrupt handler
+typedef struct
+{
+    uint32_t ds;
+    uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax; // pusha
+    uint32_t interruptNumber, errorCode;            // interrupt info
+    uint32_t eip, cs, eflags, user_esp, ss;         // pushed by processor
+} interrupt_handler_stack_t;
+
+// function handles all interrupts.
+// input: different data pushed to stack (interrupt_handler_stack_t)
+// return: none
+void isr_handler(interrupt_handler_stack_t info);
+
+// Interrupt service routines
+extern void isr0();
+extern void isr1();
+extern void isr2();
+extern void isr3();
+extern void isr4();
+extern void isr5();
+extern void isr6();
+extern void isr7();
+extern void isr8();
+extern void isr9();
+extern void isr10();
+extern void isr11();
+extern void isr12();
+extern void isr13();
+extern void isr14();
+extern void isr15();
+extern void isr16();
+extern void isr17();
+extern void isr18();
+extern void isr19();
+extern void isr20();
+extern void isr21();
+extern void isr22();
+extern void isr23();
+extern void isr24();
+extern void isr25();
+extern void isr26();
+extern void isr27();
+extern void isr28();
+extern void isr29();
+extern void isr30();
+extern void isr31();
 
 #endif
--- kernel/interrupts.asm
+++ kernel/interrupts.asm
@@ -7,3 +7,84 @@ load_idt:
     mov eax, [esp + 4]  ; idt pointer param
     lidt [eax]
     ret
+
+; common isr handler. saves state and sets up kernel mode segments.
+extern isr_handler  ; c implementation
+isr_common_handler:
+    pusha
+
+    ; save current data segment
+    mov ax, ds
+    push eax
+
+    ; set kernel mode data segment
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    call isr_handler
+
+    pop eax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    popa
+    add esp, 8
+    sti
+    iret
+
+; Interrupt Service Routines
+
+%macro ISR_NO_ERROR_CODE 1
+global isr%1
+isr%1:
+    cli
+    push byte 0     ; dummy error code
+    push byte %1    ; interrupt number
+    jmp isr_common_handler
+%endmacro
+
+%macro ISR_ERROR_CODE 1
+global isr%1
+isr%1:
+    cli
+    push byte %1    ; interrupt number
+    jmp isr_common_handler
+%endmacro
+
+ISR_NO_ERROR_CODE 0
+ISR_NO_ERROR_CODE 1
+ISR_NO_ERROR_CODE 2
+ISR_NO_ERROR_CODE 3
+ISR_NO_ERROR_CODE 4
+ISR_NO_ERROR_CODE 5
+ISR_NO_ERROR_CODE 6
+ISR_NO_ERROR_CODE 7
+ISR_ERROR_CODE 8
+ISR_NO_ERROR_CODE 9
+ISR_ERROR_CODE 10
+ISR_ERROR_CODE 11
+ISR_ERROR_CODE 12
+ISR_ERROR_CODE 13
+ISR_ERROR_CODE 14
+ISR_NO_ERROR_CODE 15
+ISR_NO_ERROR_CODE 16
+ISR_NO_ERROR_CODE 17
+ISR_NO_ERROR_CODE 18
+ISR_NO_ERROR_CODE 19
+ISR_NO_ERROR_CODE 20
+ISR_NO_ERROR_CODE 21
+ISR_NO_ERROR_CODE 22
+ISR_NO_ERROR_CODE 23
+ISR_NO_ERROR_CODE 24
+ISR_NO_ERROR_CODE 25
+ISR_NO_ERROR_CODE 26
+ISR_NO_ERROR_CODE 27
+ISR_NO_ERROR_CODE 28
+ISR_NO_ERROR_CODE 29
+ISR_NO_ERROR_CODE 30
+ISR_NO_ERROR_CODE 31
